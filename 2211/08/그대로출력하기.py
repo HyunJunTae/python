39e1dd823e53f75d69c36c15e1fb7f170df2d232
HyunJunTae <juny000041@gmail.com>
2022-11-08 22:47:33 +0900

    221108

---
+++ 2211/08/그대로출력하기.py
@@ -0,0 +1,17 @@
+# r = []
+# for i in range (100) :
+#     k = input()
+#     if (k == '') :
+#         break
+#     r.append(k)
+
+# for i in range (len(r)) :
+#     print(r[i])
+    
+    
+while(1) :
+    try:
+        print(input())
+    except EOFError:
+        break
+        
